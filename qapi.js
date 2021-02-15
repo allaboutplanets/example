d8e90dcd8546d2af6cf9f27c923b381c0b01d171
allaboutplanets <78298766+allaboutplanets@users.noreply.github.com>
2021-02-15 10:21:22 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6029fd4971e66d001efb378e",
+				"6029fd4971e66d001efb3790"
+			]
+		},
+		"6029fd4971e66d001efb378e": {
+			"id": "6029fd4971e66d001efb378e",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6029fd4971e66d001efb3790": {
+			"id": "6029fd4971e66d001efb3790",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
